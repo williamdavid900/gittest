1bad0db5c80782a58b681f6c82b366c8807336fc
williamdavid900 <wtrenker@gmail.com>
2024-04-09 10:01:33 -0700

    goland

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
 
 	"golang.org/x/example/hello/reverse"
 )
 
 func main() {
-	fmt.Println(reverse.String("Hello There Janice"))
+	fmt.Println(reverse.String("Hello There Janice Trenker"))
+	fmt.Println("try git, again.", bits.Reverse64(123))
 }
